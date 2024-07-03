3e8aa3f052e5cd774d309f7d6fe2038145ae0e8b
workprior <lolprior.201@gmail.com>
2024-07-03 18:05:21 +0300

    fix: task 1: remove test->folders; add poetry

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
-
+import numpy
+from math import pi
 app = FastAPI()
-
-
+bs5 = bs4.BeautifulSoup()
+print("Hello World")
+print(pi)
 @app.get("/")
 def root():
     return {
