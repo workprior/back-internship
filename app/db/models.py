8717014b07fe7d6b84b54fadcb0980f6598f304f
workprior <lolprior.201@gmail.com>
2024-07-24 23:17:10 +0300

    fix: add fields to user table and remove empty migrations

--- app/db/models.py
+++ app/db/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Boolean, Column, Integer, String
 
 from app.db.postgres_init import Base
 
@@ -9,14 +9,13 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     firstname = Column(String, nullable=False)
     hashed_password = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    is_active = Column(Boolean, default=True)
+    lastname = Column(String, nullable=False)
+    city = Column(String)
+    phone = Column(String, unique=True)
 
-    # email = Column(String, unique=True, index=True, nullable=False)
-    # is_active = Column(Boolean, default=True)
-    # lastname = Column(String, nullable=False)
-    # city = Column(String)
-    # phone = Column(String, unique=True)
-
-    # is_superuser = Column(Boolean, default=False)
+    is_superuser = Column(Boolean, default=False)
 
     def __repr__(self):
         return f"Record('{self.firstname}')"
--- migrations/versions/5ceb1b2a63bfbe_first_migration.py
+++ migrations/versions/5ceb1b2a63bfbe_first_migration.py
@@ -13,8 +13,8 @@ import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = "0ef51e3044be"
-down_revision: Union[str, None] = "4876c86fce38"
+revision: str = "5ceb1b2a63bf"
+down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
